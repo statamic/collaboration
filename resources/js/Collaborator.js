a3f93eb9575bbb3c1d17e09212d409e4b9fda06e
Jason Varga <jason@pixelfear.com>
2019-04-12 11:43:46 -0400

    wip

---
+++ resources/js/Collaborator.js
@@ -0,0 +1,58 @@
+export default class Collaborator {
+
+    constructor() {
+        this.debouncedBroadcastValueChange = _.debounce(function (payload) {
+            this.broadcastValueChange(payload);
+        }, 500);
+    }
+
+    start() {
+        if (this.echo && this.container) this.initializeEcho();
+    }
+
+    initializeEcho() {
+        const reference = this.container.reference.replace('::', '.');
+        const name = `${reference}.${this.container.site}`;
+
+        this.channel = this.echo.join(name);
+
+        this.channel.here(users => {
+            this.subscribeToVuexMutations();
+            const names = users.map(user => user.name).join(' ');
+            Statamic.$notify.success(`Users here: ${names}`);
+        });
+
+        this.channel.joining(user => {
+            Statamic.$notify.success(`${user.name} has joined.`);
+        });
+
+        this.channel.leaving(user => {
+            Statamic.$notify.success(`${user.name} has left.`);
+        });
+
+        this.channel.listenForWhisper('updated', e => {
+            this.applyBroadcastedValueChange(e);
+        });
+    }
+
+    subscribeToVuexMutations() {
+        Statamic.$store.subscribe((mutation, state) => {
+            if (mutation.type === `publish/${this.container.name}/setValue`) {
+                this.debouncedBroadcastValueChange(mutation.payload);
+            }
+        });
+    }
+
+    broadcastValueChange({ handle, value, user }) {
+        // Only my own change events should be broadcasted. Otherwise when other users receive
+        // the broadcast, it will be re-broadcasted, and so on, to infinity and beyond.
+        if (Statamic.$config.get('userId') == user) {
+            this.channel.whisper('updated', { handle, value, user });
+        }
+    }
+
+    applyBroadcastedValueChange(payload) {
+        Statamic.$store.dispatch(`publish/${this.container.name}/setValue`, payload);
+    }
+
+}
--- resources/js/collaboration.js
+++ resources/js/collaboration.js
@@ -1,45 +1,13 @@
+import Collaborator from './Collaborator';
+const collaborator = new Collaborator;
+
 Statamic.$echo.booted(Echo => {
-
-    const id = 'f5c18e4c-4d51-4fc6-ab52-b7afe5116b3a';
-    const channel = `entries.${id}`;
-    const storeName = 'base';
-
-    Echo.join(channel)
-        .here((users) => {
-            watchForVuexChanges();
-            const names = users.map(user => user.name).join(' ');
-            Statamic.$notify.success(`Users here: ${names}`);
-        })
-        .joining((user) => {
-            Statamic.$notify.success(`${user.name} has joined.`);
-        })
-        .leaving((user) => {
-            Statamic.$notify.success(`${user.name} has left.`);
-        });
-
-    Echo.private(channel).listenForWhisper('updated', e => {
-        applyBroadcastedValueChange(e);
-    });
-
-    function watchForVuexChanges() {
-        Statamic.$store.subscribe((mutation, state) => {
-            if (mutation.type === `publish/${storeName}/setValue`) {
-                broadcastValueChange(mutation.payload);
-            }
-        });
-    }
-
-    const broadcastValueChange = _.debounce(function ({ handle, value, user }) {
-        // Only my own change events should be broadcasted. Otherwise when other users receive
-        // the broadcast, it will be re-broadcasted, and so on, to infinity and beyond.
-        if (Statamic.$config.get('userId') != user) return;
-
-        Echo.private(channel)
-            .whisper('updated', { handle, value, user });
-    }, 500);
-
-    function applyBroadcastedValueChange({ handle, value, user }) {
-        Statamic.$store.dispatch(`publish/${storeName}/setValue`, { handle, value, user });
-    }
-
+    collaborator.echo = Echo;
+    collaborator.start();
 });
+
+Statamic.$events.$on('publish-container-created', container => {
+    collaborator.container = container;
+    collaborator.start();
+});
+
