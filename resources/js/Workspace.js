b19731a5834e5a5bc0974510957684766d9b53b6
Jason Varga <jason@pixelfear.com>
2019-04-12 15:35:33 -0400

    Handle preventing broadcasting when there are no changes.

--- resources/js/Workspace.js
+++ resources/js/Workspace.js
@@ -5,6 +5,7 @@ export default class Workspace {
         this.echo = null;
         this.started = false;
         this.storeSubscriber = null;
+        this.lastValues = {};
 
         this.debouncedBroadcastValueChange = _.debounce(function (payload) {
             this.broadcastValueChange(payload);
@@ -49,21 +50,53 @@ export default class Workspace {
 
     subscribeToVuexMutations() {
         this.storeSubscriber = Statamic.$store.subscribe((mutation, state) => {
-            if (mutation.type === `publish/${this.container.name}/setValue`) {
-                this.debouncedBroadcastValueChange(mutation.payload);
+            switch (mutation.type) {
+                case `publish/${this.container.name}/setValue`:
+                    this.vuexValueHasBeenSet(mutation.payload);
             }
         });
     }
 
-    broadcastValueChange({ handle, value, user }) {
+    // A value has been set in the vuex store.
+    // It could have been triggered by the current user editing something,
+    // or by the workspace applying a change dispatched by another user editing something.
+    vuexValueHasBeenSet(payload) {
+        this.debug('Vuex value has been set', payload);
+        if (!this.valueHasChanged(payload.handle, payload.value)) {
+            // No change? Don't bother doing anything.
+            this.debug(`Value for ${payload.handle} has not changed.`, { value: payload.value, lastValue: this.lastValues[payload.handle] });
+            return;
+        }
+
+        this.rememberValueChange(payload.handle, payload.value);
+        this.debouncedBroadcastValueChange(payload);
+    }
+
+    rememberValueChange(handle, value) {
+        this.debug('Remembering value change', { handle, value });
+        this.lastValues[handle] = clone(value);
+    }
+
+    valueHasChanged(handle, newValue) {
+        const lastValue = this.lastValues[handle] || null;
+        return JSON.stringify(lastValue) !== JSON.stringify(newValue);
+    }
+
+    broadcastValueChange(payload) {
         // Only my own change events should be broadcasted. Otherwise when other users receive
         // the broadcast, it will be re-broadcasted, and so on, to infinity and beyond.
-        if (Statamic.$config.get('userId') == user) {
-            this.channel.whisper('updated', { handle, value, user });
+        if (Statamic.$config.get('userId') == payload.user) {
+            this.debug('📣 Broadcasting', payload);
+            this.channel.whisper('updated', payload);
         }
     }
 
     applyBroadcastedValueChange(payload) {
+        this.debug('✅ Applying broadcasted change', payload);
         Statamic.$store.dispatch(`publish/${this.container.name}/setValue`, payload);
+    }
+
+    debug(message, args) {
+        console.log('[Collaboration]', message, {...args});
     }
 }
