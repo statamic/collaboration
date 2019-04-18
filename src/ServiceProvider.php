09c06821be97495ddb8e64083888e33af2a2e17e
Jason Varga <jason@pixelfear.com>
2019-04-18 13:40:11 -0400

    Add a broadcast channel

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,9 +2,19 @@
 
 namespace Statamic\Collaboration;
 
+use Illuminate\Support\Facades\Broadcast;
 use Statamic\Extend\ServiceProvider as BaseProvider;
 
 class ServiceProvider extends BaseProvider
 {
     protected $scripts = [__DIR__.'/../resources/js/collaboration.js'];
+
+    public function boot()
+    {
+        parent::boot();
+
+        Broadcast::channel('entry.{id}.{site}', function ($user, $id, $site) {
+            return $user->toArray();
+        });
+    }
 }
