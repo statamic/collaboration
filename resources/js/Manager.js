3f6ac51832d919c6a498ef514a9efafef43d789b
Wiktor Mrówczyński <wiktor@4dmind.com>
2026-05-14 21:43:37 +0200

    Add EmptyWorkspace

--- resources/js/Manager.js
+++ resources/js/Manager.js
@@ -1,3 +1,4 @@
+import EmptyWorkspace from './EmptyWorkspace';
 import Workspace from './Workspace';
 
 export default class Manager {
@@ -10,13 +11,37 @@ export default class Manager {
     boot() {
         if (! this.echo) return;
 
+        let workspaceStarted = false;
+
+
         Object.values(this.workspaces).forEach(workspace => {
             workspace.echo = this.echo;
             workspace.start();
+
+            workspaceStarted = true;
         });
+
+        if( !workspaceStarted ) {
+            this.addEmptyWorkspace();
+        }
+    }
+
+    addEmptyWorkspace() {
+        const workspace = new EmptyWorkspace();
+        this.workspaces['empty'] = workspace;
+        this.boot();
+    }
+
+    destroyEmptyWorkspace() {
+        this.workspaces['empty'].destroy();
+        delete this.workspaces['empty'];
     }
 
     addWorkspace(container) {
+        if( this.workspaces['empty'] ) {
+            this.destroyEmptyWorkspace();
+        }
+
         const workspace = new Workspace(container);
         this.workspaces[container.name] = workspace;
         this.boot();
